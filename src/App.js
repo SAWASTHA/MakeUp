fa7e7d2de0b49d8b2f96f70ee08abae253488979
Rafat <shaikhrafat25@gmail.com>
2024-05-14 15:47:13 +0530

    Added Collage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Component  from './components/Info';
+import Collage from './components/Collage';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar  title="Preet Makeup"/>  
+      <Collage/>
+      <Component/>
+
     </div>
   );
 }
